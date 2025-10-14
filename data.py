d24187636529e33e3cf5df43b176b401257d328b
Alaqmar Gandhi <alaqmargandhi@gmail.com>
2025-10-13 23:17:14 -0400

    refactor: Improve data loading and solution matching in get_data function

--- data.py
+++ data.py
@@ -11,19 +11,20 @@ def get_data(limit=-1):
     data_set = {}
 
     with open("./benchmark/arc-agi_challenges.json") as file:
-        file = json.load(file)
+        challenges = json.load(file)
 
-        for id, data in list(file.items()) if limit == -1 else list(file.items())[:limit]:
-            data_set[id] = Data(id, data['train'], data['test'], None)
+        for id, challenge_data in list(challenges.items()) if limit == -1 else list(challenges.items())[:limit]:
+            data_set[id] = Data(id, challenge_data['train'], challenge_data['test'], None)
 
     with open("./benchmark/arc-agi_solutions.json") as file:
-        file = json.load(file)
+        solutions = json.load(file)
 
-        for id, data in list(file.items()) if limit == -1 else list(file.items())[:limit]:
-            if data_set[id]:
-                for index, value in enumerate(data_set[id].test):
-                    value['output'] = data[index]
-            else:
-                error("Missing Solutions for available challenges")
+        # Then, match solutions to their test cases
+        for id in data_set.keys():
+            if id in solutions:
+                solution_data = solutions[id]
+                
+                for test_case, solution in zip(data_set[id].test, solution_data):
+                    test_case['output'] = solution
 
     return data_set
